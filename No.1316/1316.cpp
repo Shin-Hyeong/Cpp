1834ef5ecc3ce8df0699463c790f4dcc2606b9ea
ShinHyoeng <struct4996@naver.com>
2023-05-04 01:01:59 +0900

    BaekJoon_230504

--- No.1316/1316.cpp
+++ No.1316/1316.cpp
@@ -3,28 +3,33 @@
 using namespace std;
 
 int main() {
-    int num , k = 1, count = 0, index = 0 , num1 = 0;
-    //string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    string str;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int count , num , r = 0;
+    string str , str1;
     cin >> num;
-
+    count = num;
     for(int i = 0; i < num; i++){
         cin >> str;
-        for(int j = 0; i < str.length() - 1; j++){
-            switch(1){
-                if(str[j] == str[j + k]){
-                    num1++;
-                    k++;
-                }
-                else if(str[j] != str[j + k]){
-                    str.erase(j + 1, k);
-                    k = 1;
-                    break;
-                }
+        str1.clear();
+        for(int j = 0; j < str.length(); j++){
+            if(str[j] != str[j + 1]){ //다음 알파벳이 다르면
+                str1.push_back(str[j]); //str1문자열에 str[j]추가
             }
         }
+        // str1 문자열에 동일 문자가 있는지 확인
+        for(int j = 0; j < str1.length() - 1; j++){
+            for(int k = j + 1; k < str1.length(); k++){
+                if(str1[j] == str1[k]) r++;
+            }
+        }
+        if(r > 0){
+            count--;
+            r = 0;
+        } 
     }
 
-    cout << str;
+    cout << count << endl;
     return 0;
 }

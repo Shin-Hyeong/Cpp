8f701909d05c9bbfb41dc7f4a44393616b2f97dc
ShinHyoeng <struct4996@naver.com>
2023-08-12 20:51:17 +0900

    BeakJoon_230812

--- No.2108/2108.cpp
+++ No.2108/2108.cpp
@@ -2,10 +2,6 @@
 #include <array>
 #include <algorithm>
 #include <functional>
-
-#define MAX(A, B) A > B ? A : B
-#define MIN(A, B) A < B ? A : B
-
 using namespace std;
 
 int main()
@@ -13,7 +9,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    int n, maxElem;
+    int n;
     double avg = 0;
     array<int, 8001> numCount = {0};
     array<int, 8001>::iterator maxIndex;
@@ -27,21 +23,20 @@ int main()
         numCount[arr[i] + 4000]++;
     }
 
+    /*산술평균*/
     avg /= n; 
     avg > 0 ? avg += 0.5 : avg -= 0.5;
-
-    /*산술평균*/
     cout << (int)avg << '\n';
 
-    sort(arr, arr + n, greater<>());
-
     /*중앙값*/
+    sort(arr, arr + n, greater<>());
     cout << arr[n / 2] << '\n';
 
     /*최빈값*/
     maxIndex = max_element(numCount.begin(), numCount.end()); 
 
-    if (*maxIndex != *max_element(maxIndex + 1, numCount.end())) 
+    if (*maxIndex != *max_element(maxIndex + 1/*maxIndex보다 큰 수*/, numCount.end()))
+    /*maxIndex 다음으로 큰 수 -> 최빈값중 두번째로 작은 값*/
         cout << (int)(maxIndex - numCount.begin()) - 4000 << "\n";
     else
         cout << (int)(max_element(maxIndex + 1, numCount.end()) - numCount.begin()) - 4000 << "\n";

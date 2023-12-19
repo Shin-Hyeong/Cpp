aed9df28afa9cb8e0c926dfb33d12d73aaae5cc4
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2023-12-19 22:42:05 +0900

    BeakJoon_231219

---
+++ No.10610/10610.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<int> nums;
+
+	int check = 0;
+
+	string N;
+	cin >> N;
+
+	for (int i = 0; i < N.size(); i++)
+	{
+		nums.push_back(N[i] - '0');
+	}
+
+	sort(nums.begin(), nums.end(), greater<>());
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		check += nums[i];
+	}
+
+	if (nums[nums.size() - 1] == 0 && check % 3 == 0)
+		for (int i = 0; i < nums.size(); i++) cout << nums[i];
+	else cout << -1;
+
+	return 0;
+}

d1817a3c42363a8c4f30ac9c934cd4c55f276183
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2024-02-04 11:12:21 +0900

    Connection Count
    https://www.acmicpc.net/problem/11724

---
+++ No.11724/BeakJoon.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#define MAX 1001
+using namespace std;
+
+int N, M, Connected = 0;
+vector<int> map[MAX];
+bool checked[MAX] = { false };
+
+void ConnectedCheck(int node);
+
+int main()
+{
+	cin >> N >> M;
+	int line1, line2;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> line1 >> line2;
+		map[line1].push_back(line2);
+		map[line2].push_back(line1);
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (checked[i] == false)
+		{
+			Connected++;
+			ConnectedCheck(i);
+		}
+	}
+
+	cout << Connected << endl;
+
+	return 0;
+}
+
+void ConnectedCheck(int node)
+{
+	checked[node] = true;
+	for (int i = 0; i < map[node].size(); i++)
+	{
+		int idx = map[node][i];
+		if (checked[idx] == false)
+		{
+			ConnectedCheck(idx);
+		}
+	}
+}

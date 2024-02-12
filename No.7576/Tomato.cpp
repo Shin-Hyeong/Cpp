b2a0718ce1a27a0942e7a437a651470786781f64
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2024-02-12 12:58:16 +0900

    Tomato
    https://www.acmicpc.net/problem/7576

---
+++ No.7576/Tomato.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <queue>
+#define MAX 1001
+using namespace std;
+
+queue<pair<int, int>> q;
+int dy[4] = { 1, -1, 0, 0 };
+int dx[4] = { 0, 0, -1, 1 };
+int y, x, Day = 0;
+int map[MAX][MAX];
+
+void BFS();
+bool IsInside(int Nexty, int Nextx);
+
+int main()
+{
+	cin >> x >> y;
+
+	for (int i = 0; i < y; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			cin >> map[i][j];
+			if (map[i][j] == 1)
+			{
+				q.push({ i, j });
+			}
+		}
+	}
+
+	BFS();
+
+	for (int i = 0; i < y; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			if (map[i][j] == 0)
+			{
+				cout << "-1\n";
+				return 0;
+			}
+			if (Day < map[i][j])
+			{
+				Day = map[i][j];
+			}
+		}
+	}
+
+	cout << Day - 1 << endl;
+	return 0;
+}
+
+void BFS()
+{
+	while (!q.empty())
+	{
+		int x = q.front().second;
+		int y = q.front().first;
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int Nexty = y + dy[i];
+			int Nextx = x + dx[i];
+
+			if (IsInside(Nexty, Nextx) && map[Nexty][Nextx] == 0)
+			{
+				map[Nexty][Nextx] = map[y][x] + 1;
+				q.push({ Nexty, Nextx });
+			}
+		}
+	}
+}
+
+bool IsInside(int Nexty, int Nextx)
+{
+	return (Nexty >= 0 && Nextx >= 0 && Nexty < y && Nextx < x);
+}
